58cc0fd443111510cb22f9c4c6c6929ea4bc632f
Luqman <62147517+luqman-haqeem@users.noreply.github.com>
2024-10-01 10:27:46 +0800

    update  PWA cinfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,17 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from 'next-themes'
 import Layout from "@/components/layout";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker
+                .register('/sw.js')
+                .then((reg) => console.log('Service Worker registered'))
+                .catch((err) => console.error('Service Worker registration failed', err));
+        }
+    }, []);
     return (
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <Layout>
