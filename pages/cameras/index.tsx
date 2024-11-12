58ef168c4d4898905a2715f6c71e397616aeba7a
Luqman <62147517+luqman-haqeem@users.noreply.github.com>
2024-11-12 08:51:54 +0800

    disabled  img  uploader

--- pages/cameras/index.tsx
+++ pages/cameras/index.tsx
@@ -157,17 +157,19 @@ export default function Component({ cameras }: ComponentProps) {
                                     </CardHeader>
                                     <CardContent className="p-4 pt-0">
                                         <div onClick={() =>
-                                            openFullscreen(`${bucketUrl}/images/${camera?.jps_camera_id}.jpg`)}
+                                            // openFullscreen(`${bucketUrl}/images/${camera?.jps_camera_id}.jpg`)}
+                                            openFullscreen(`/api/proxy-image/${camera?.jps_camera_id}`)}
                                             className="relative cursor-pointer">
                                             <Image
-                                                src={`${bucketUrl}/images/${camera?.jps_camera_id}.jpg`}
-                                                // src={`/api/proxy-image/${camera?.jps_camera_id}`}
+                                                // src={`${bucketUrl}/images/${camera?.jps_camera_id}.jpg`}
+                                                src={`/api/proxy-image/${camera?.jps_camera_id}`}
                                                 width={600}
                                                 height={330}
                                                 alt={`${camera.camera_name} feed`}
                                                 className="w-full rounded-md"
                                                 onError={(e) => e.currentTarget.src = '/nocctv.png'}
                                                 blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAUAB4DASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD0ykZgoyxAHqaomO5xhVkAOM7pMnPOT16dO/4UrxXDqm4OWwhyHAAIxnI9aALwIIyDkGiqaxTqA5Z92TnLZGNvp9aLKTezkFyoVfvPu55z/SgC5RRRQAUUUUAf/9k="
+                                                unoptimized
                                                 placeholder="blur"
                                             ></Image>
                                             <div className="absolute top-0 right-0 m-2">
